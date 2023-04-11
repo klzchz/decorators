74c458478845cd914ddff615a6ecb6397bcb37c4
Lucas Andrade <lucascorporation@live.com>
2023-04-11 20:53:03 -0300

    property

---
+++ src/property.ts
@@ -0,0 +1,43 @@
+
+
+
+const  minLength = (length:number)=> {
+    return (target:any,key:string)=>{
+        let val = target[key];
+        const getter = () => val;
+        const setter = (value:string)=>{
+            if(value.length < length)
+            {
+                throw new Error(`Value for ${key} need more then ${length}`)
+            }
+            val = value;
+        }
+
+        Object.defineProperty(target,key,{
+            get:getter,
+            set:setter
+        });
+
+    }
+}
+
+
+
+
+
+class Category { 
+    @minLength(3)
+    private title:string;
+
+
+    constructor(t:string) {
+        this.title = t;
+
+    }
+
+}
+
+console.log(new Category('t'));
+
+
+export default {};
